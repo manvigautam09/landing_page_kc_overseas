0fa02b7a38bbfac07390724bbb0b569f39185608
manvigautam09 <mnvsgautam9@gmail.com>
2025-02-10 03:29:45 +0530

    Fixes in header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 import logo from "../../assets/logo.svg";
+import close from "../../assets/close.svg";
+import hamburger from "../../assets/hamburger.svg";
 
 const navItems = [
   { label: "Study Destinations" },
@@ -10,16 +14,46 @@ const navItems = [
 ];
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <header className="absolute top-0 left-0 right-0 flex items-center h-20 px-12 py-2 justify-between">
       <Image src={logo} alt="logo" width={205} height={45} />
-      <div className="flex space-x-10 text-white font-semibold text-base items-center">
+      <Image
+        src={hamburger}
+        alt="hamburger"
+        className="lg:hidden cursor-pointer z-10"
+        onClick={() => setIsMenuOpen(true)}
+      />
+
+      <div className="hidden lg:flex flex-wrap space-x-10 text-white font-semibold text-base items-center">
         {navItems.map((item) => (
           <nav key={item.label}>{item.label}</nav>
         ))}
         <button className="border border-white rounded px-4 py-2">
           Course Finder
         </button>
+      </div>
+      <div
+        className={`${
+          isMenuOpen ? "translate-y-0 backdrop-blur-sm" : "-translate-y-full"
+        } fixed top-0 right-0 bg-[#6C48F0] z-50 px-8 py-8 w-full ease-in-out duration-500 lg:hidden`}
+      >
+        <button
+          type="button"
+          onClick={() => setIsMenuOpen(false)}
+          className="flex justify-end ml-auto"
+          title="Close Menu"
+        >
+          <Image src={close} alt="close" />
+        </button>
+        <div className="flex flex-col gap-8 text-lg items-center text-white font-semibold">
+          {navItems.map((item) => (
+            <nav key={item.label}>{item.label}</nav>
+          ))}
+          <button className="border border-white rounded px-4 py-2">
+            Course Finder
+          </button>
+        </div>
       </div>
     </header>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import Header from "./components/Header";
 import landingSvg from "./assets/landing-bg.svg";
+import landingMobileBg from "./assets/landing-mobile-bg.svg";
 import arrowDown from "./assets/arrow-down.svg";
 import Services from "./components/Services";
 import Ambitions from "./components/Ambitions";
@@ -14,22 +15,32 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div className="relative w-screen h-screen overflow-scroll">
-      <Image src={landingSvg} alt="landing-bg" className="bg-cover w-full" />
+      <Image
+        src={landingSvg}
+        alt="landing-bg"
+        className="bg-cover hidden sm:block"
+      />
+      <Image
+        src={landingMobileBg}
+        alt="landing-mobile-bg"
+        className="w-full sm:hidden max-h-[720px] "
+      />
+
       <Header />
-      <div className="absolute top-[100px] flex flex-col items-center justify-center text-center px-4 w-full text-white">
-        <div className="flex items-center gap-2 my-4 font-semibold text-base">
+      <div className="absolute top-[100px] sm:top-[40px] lg:top-[100px] flex flex-col items-center lg:justify-center text-center px-4 w-full text-white">
+        <div className="flex items-center gap-2 xl:my-4 font-semibold text-base">
           <span className="opacity-50">Home</span>
           <span>•</span>
           <span>Pune Branch</span>
         </div>
 
-        <div className="font-semibold text-4xl mt-4">
+        <div className="font-semibold text-3xl md:text-2xl lg:text-3xl xl:text-4xl mt-4">
           <span>KC Overseas Education </span>
           <br />
           Pune
         </div>
 
-        <div className="max-w-3xl text-lg mb-8">
+        <div className="max-w-3xl sm:text-sm lg:text-lg mb-8">
           We value your career aspirations, which is why we map your preferences
           with the best that <br />
           our global universities have to offer. Thousands of our students are
@@ -37,12 +48,12 @@ export default function Home() {
           globally and we welcome you to pursue yours!
         </div>
 
-        <div className="flex gap-4">
+        <div className="flex gap-4 flex-wrap justify-center">
           <button
             style={{
               backgroundImage: "linear-gradient(to right, #FF6A56, #FDAF4D)",
             }}
-            className="px-6 rounded-md text-white font-bold hover:shadow-lg hover:shadow-gray-400"
+            className="min-h-12 px-6 rounded-md text-white font-bold hover:shadow-lg hover:shadow-gray-400"
           >
             Enquire Now
           </button>
@@ -57,16 +68,16 @@ export default function Home() {
       </div>
 
       {/* Services Section */}
-      {/* <Services /> */}
+      <Services />
 
       {/* Global Ambitions Section */}
-      {/* <Ambitions /> */}
+      <Ambitions />
 
       {/* 7 Easy Steps Section */}
-      {/* <Steps /> */}
+      <Steps />
 
       {/* Webinars & Events Section */}
-      {/* <WebinarsAndEvents /> */}
+      <WebinarsAndEvents />
 
       {/* Reviews Section */}
       <Reviews />
