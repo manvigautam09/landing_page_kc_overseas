f4590cb3035536a70c8a622cc8c08930540280c1
manvigautam09 <mnvsgautam9@gmail.com>
2025-02-10 01:27:50 +0530

    Adds reusable component

--- app/components/Reviews/index.tsx
+++ app/components/Reviews/index.tsx
@@ -1,34 +1,47 @@
+import Image from "next/image";
 import quotes from "../../assets/quotes.svg";
 
 const Reviews = () => {
+  const studentReviews = [
+    {
+      message:
+        "Thank you, KC, for all the support you have provided right from shortlisting the university to the visa interview preparations. I really admire the attention and support of the KC Counsellors.",
+      name: "Mandar Suryaawanshi",
+      institution: "Northeastern University, USA",
+    },
+    {
+      message:
+        "The team made the entire process very easy. Everyone is very helpful and cooperative throughout. I am thankful to KC Sadar Team for making my dream come true.",
+      name: "Aarchi Sighat",
+      institution: "University of South Australia, Australia",
+    },
+    {
+      message:
+        "Helped with counseling and making the right decision...great staff..would definitely recommend it to anyone planning to go abroad for studies..",
+      name: "Susan Mendonca",
+      institution: "Conestoga College, Canada",
+    },
+  ];
+
   return (
     <section className="bg-[#FFDCD8] py-12">
       <h2 className="text-center text-2xl font-bold">Our students love us</h2>
       <div className="flex justify-center mt-8 space-x-4">
-        <div className="bg-white p-6 rounded-lg shadow-md">
-          <p>
-            Thank you, KC, for all the support you have provided right from
-            shortlisting the university to the visa interview preparation.
-          </p>
-          <p className="mt-4 font-semibold">Mandar Suryaawanshi</p>
-          <p>Northeastern University, USA</p>
-        </div>
-        <div className="bg-white p-6 rounded-lg shadow-md">
-          <p>
-            The team made the entire process very easy. Everyone is very helpful
-            and cooperative throughout.
-          </p>
-          <p className="mt-4 font-semibold">Aarchi Sighat</p>
-          <p>University of South Australia, Australia</p>
-        </div>
-        <div className="bg-white p-6 rounded-lg shadow-md">
-          <p>
-            Helped with counseling and making the right decision...great
-            staff...would definitely recommend.
-          </p>
-          <p className="mt-4 font-semibold">Susan Mendonca</p>
-          <p>Conestoga College, Canada</p>
-        </div>
+        {studentReviews.map((review, index) => (
+          <div
+            key={index}
+            className="bg-white p-6 rounded-lg shadow-md w-[300px] h-[300px] flex flex-col justify-between"
+          >
+            <div>
+              <Image src={quotes} alt="Quotes" />
+              <p className="w-[250px] mt-4">{review.message}</p>
+            </div>
+            <div>
+              <p className="mt-4 font-semibold">{review.name}</p>
+              <p className="text-[#64647A] text-sm">{review.institution}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
